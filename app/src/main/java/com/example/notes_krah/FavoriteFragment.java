c38d8c038c65cb7769e7104f59947dcd554c61b3
stolveron <natasinuk@mail.ru>
2021-07-28 18:28:37 +0300

    Боковое меню есть. Navigation drawer, где аватарка и тд есть. Toast есть. Редактировать данные о заметке реализовано без сохранения, не считая дату, которая только отображается в редактировании.

---
+++ app/src/main/java/com/example/notes_krah/FavoriteFragment.java
@@ -0,0 +1,67 @@
+package com.example.notes_krah;
+
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.fragment.app.Fragment;
+
+public class FavoriteFragment extends Fragment {
+
+    public static final String ARG_NOTE = "note";
+    private Note note;
+
+    // Фабричный метод создания фрагмента
+    // Фрагменты рекомендуется создавать через фабричные методы.
+    public static FavoriteFragment newInstance(Note note) {
+        FavoriteFragment f = new FavoriteFragment();    // создание
+
+        // Передача параметра
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_NOTE, note);
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            note = getArguments().getParcelable(ARG_NOTE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Таким способом можно получить головной элемент из макета
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        //теперь найдем и будем работать с нужними нам элементами
+        EditText editTextViewName = view.findViewById(R.id.EditTextViewName);
+        EditText editTextViewBody = view.findViewById(R.id.EditTextViewBody);
+        TextView textViewDate = view.findViewById(R.id.textViewDate);
+        DatePicker datePicker = view.findViewById(R.id.datePicker);
+        editTextViewName.setText(note.getNoteName(getContext()));
+        editTextViewBody.setText(note.getNoteBody(getContext()));
+        // по хорошему нужно вот так запивать textViewDate.setText(new StringBuilder().append(note.getNoteDateYear(getContext())).append(" ").append(note.getNoteDateMonth(getContext())).append(" ").append(note.getNoteDateDay(getContext())).toString());
+        textViewDate.setText(note.getNoteDateYear(getContext())+" "+ note.getNoteDateMonth(getContext())+" "+ note.getNoteDateDay(getContext()));
+        datePicker.init(note.getNoteDateYear(getContext()), note.getNoteDateMonth(getContext()), note.getNoteDateDay(getContext()), new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                //сохраним полученные данные
+                note.setNoteDate(getContext(),year, monthOfYear,dayOfMonth);
+                // обновим поле даты
+                textViewDate.setText(new StringBuilder().append(note.getNoteDateYear(getContext())).append(" ").append(note.getNoteDateMonth(getContext())).append(" ").append(note.getNoteDateDay(getContext())).toString());
+            }
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/notes_krah/MainFragment.java
@@ -0,0 +1,134 @@
+package com.example.notes_krah;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainFragment extends Fragment {
+    private boolean isLandscape;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        setHasOptionsMenu(true);
+        initPopupMenu(view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    private void initList(View view) {
+       LinearLayout linearView = (LinearLayout) view;
+        String[] notes = getResources().getStringArray(R.array.note_name);
+
+        for (int i = 0; i < notes.length; i++) {
+            String note = notes[i];
+            TextView tv = new TextView(getContext());
+            tv.setText(note);
+            tv.setTextSize(30);
+            linearView.addView(tv);
+            final int fi = i;
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //, getResources().getStringArray(R.array.note_name)[fi]
+                    ((MainActivity)getActivity()).currentNote = new Note(fi);
+                    showNoteDescription(((MainActivity)getActivity()).currentNote);
+                }
+            });
+        }
+    }
+
+
+
+    private void initPopupMenu(View view) {
+        TextView tv = new TextView(getContext());
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity activity = requireActivity();
+                PopupMenu popupMenu = new PopupMenu(activity, v);
+                activity.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+                Menu menu = popupMenu.getMenu();
+                menu.findItem(R.id.item2_popup).setVisible(false);
+                menu.add(0, 123456, 12, R.string.new_menu_item_added);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.item1_popup:
+                                Toast.makeText(getContext(), "Chosen popup item 1", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.item2_popup:
+                                Toast.makeText(getContext(), "Chosen popup item 2", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case 123456:
+                                Toast.makeText(getContext(), "Chosen new item added", Toast.LENGTH_SHORT).show();
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_add){
+            Toast.makeText(getContext(), "Chosen add", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showNoteDescription(Note currentNote) {
+        if (isLandscape) {
+            showLandNoteDescription(currentNote);
+        } else {
+            showPortNoteDescription(currentNote);
+        }
+    }
+
+    private void showLandNoteDescription(Note currentNote) {
+        FavoriteFragment detail = FavoriteFragment.newInstance(currentNote);
+
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, detail);  // замена фрагмента
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+    }
+
+    private void showPortNoteDescription(Note currentNote) {
+        Toast.makeText(getActivity()," Выбрана заметка "+currentNote.getNoteName(getActivity()),Toast.LENGTH_SHORT).show();
+    }
+}
